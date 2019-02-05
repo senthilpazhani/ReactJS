5d2121f61ad6944f9d5ca6d8be2f5d2ff007abc9
css111600 <senthilpazhani@gmail.com>
2019-02-05 20:51:44 +0530

    5th Nov

--- ES6/test.js
+++ ES6/test.js
@@ -1,5 +1,5 @@
 var empObj={name:'PSK',
 id:12,
-getName:()=>{return this.name}
+getName:function(){return this.name}
 }
 console.log('Print Name : ',empObj.getName());
--- reactjs/firstapp/src/App.js
+++ reactjs/firstapp/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state= {
+      myTitle:"Mr",
+      myName:this.props.myName,
+      btnText:"Click Me !"}
+  }
+  handleClick(){
+    this.setState={
+      btnText:"Hi I'm Clicked "
+    }
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header"> Welcome ! {this.state.myTitle} {this.state.myName} here
+          <button onClick={this.state.handleClick}>{this.state.btnText}</button>
         </header>
       </div>
     );
   }
 }
 
+App.propTypes = {
+  name: PropTypes.string,
+};
+App.defaultProps={
+  myName : "PSK"
+}
 export default App;
